bb84c4c54cb23de7a63a88a689a44787e221bcd3
Erik Hollensbe <erik@hollensbe.org>
2010-12-15 02:06:17 -0500

    * added test for earlier bugfix. * Added some copy for gem developers.

--- spec/controllers/rubygems_controller_spec.rb
+++ spec/controllers/rubygems_controller_spec.rb
@@ -8,4 +8,10 @@ describe RubygemsController do
     response.should be_successful
   end
 
+  it 'should route to the root url when showing a gem that does not exist' do
+    get :show, id: 'foo'
+    response.should be_redirect
+    flash[:notice].should == "That gem does not exist"
+    response.should redirect_to root_path 
+  end
 end
