6422c5a1d322e844693d00418dcc3f5723661e64
Erik Hollensbe <erik@hollensbe.org>
2010-12-26 17:52:57 -0500

    * Fix for missing version or rubygem when fetching for versions#show

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -6,9 +6,19 @@ class VersionsController < ApplicationController
   
   def show
     @rubygem = Rubygem.where(name: params[:rubygem_id]).first
-    @version = Version.where(number: params[:id], rubygem_id: @rubygem.id).first
-    @test_results = TestResult.where(rubygem_id: @rubygem.id, version_id: @version.id).all
-    fill_results_page    
+    if @rubygem
+      @version = Version.where(number: params[:id], rubygem_id: @rubygem.id).first
+      if @version
+        @test_results = TestResult.where(rubygem_id: @rubygem.id, version_id: @version.id).all
+      end
+    end
+
+    unless @rubygem and @version and @test_results
+      flash[:notice] = "This item could not be found"
+      redirect_to :back rescue redirect_to root_url
+    else
+      fill_results_page    
+    end
   end
 
 end
