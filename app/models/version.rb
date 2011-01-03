afa830143278669fc6f214c9f331a4867ff8b3a2
Josiah Kiehl <josiah@bluepojo.com>
2011-01-02 20:46:37 -0500

    Unique version numbers

--- app/models/version.rb
+++ app/models/version.rb
@@ -3,4 +3,6 @@ class Version < ActiveRecord::Base
   has_many :test_results
   belongs_to :rubygem
 
+  validates_uniqueness_of :number, scope: [:rubygem_id]
+  
 end
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -12,4 +12,19 @@ describe Version do
     Version.create! @valid_attributes
   end
 
+  it 'should not create 2 of the same version #s within the scope of a rubygem' do
+    gem = Factory.create :rubygem
+    Factory.create :version, number: '1.0.0', rubygem_id: gem.id
+    v = Factory.build :version, number: '1.0.0', rubygem_id: gem.id
+    v.save.should be_false
+  end
+
+  
+  it 'should allow the same version #s outside the scope of a rubygem' do
+    gem = Factory.create :rubygem
+    gem2 = Factory.create :rubygem
+    Factory.create :version, number: '1.0.0', rubygem_id: gem.id
+    v = Factory.build :version, number: '1.0.0', rubygem_id: gem2.id
+    v.save.should be_true
+  end
 end
