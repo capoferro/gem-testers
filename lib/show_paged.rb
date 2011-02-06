4e3966488ff9b08f4f2474aa9e012b6d6739d98e
Josiah Kiehl <josiah@bluepojo.com>
2011-02-06 09:54:53 -0500

    Incorrect lookup for version on show_paged

--- lib/show_paged.rb
+++ lib/show_paged.rb
@@ -7,7 +7,7 @@ module ShowPaged
     conditions.merge!(platform: params[:platform]) if params[:platform]
     
     if params[:version_id]
-      v = Version.where(number: params[:version_id]).first
+      v = Version.where(number: params[:version_id], rubygem_id: rubygem.id).first
       conditions.merge!(version_id: v.id)
     end
     
@@ -18,6 +18,7 @@ module ShowPaged
     filtered_q = q.order("#{TestResult::DATATABLES_COLUMNS[params[:iSortCol_0].to_i]} #{params[:sSortDir_0]}").matching(params[:sSearch])
 
     @filtered_count = filtered_q.count
+    pp filtered_q.offset(params[:iDisplayStart]).limit(params[:iDisplayLength]).to_sql
     @results = filtered_q.offset(params[:iDisplayStart]).limit(params[:iDisplayLength]).all
     
     render json: {iTotalRecords: @count, iTotalDisplayRecords: @filtered_count, aaData: @results.collect(&:datatables_attributes)}
