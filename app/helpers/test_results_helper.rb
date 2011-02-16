7ed8e2430853bc633f376e79c031b5315c24534d
Matte Noble <me@mattenoble.com>
2011-02-15 21:41:12 -0500

    Replacing "Select All" button with "Copy Output" (to clipboard) button

--- app/helpers/test_results_helper.rb
+++ app/helpers/test_results_helper.rb
@@ -2,4 +2,8 @@ module TestResultsHelper
   def filter_ansi(text)
     text.gsub(/\e[^a-z]+[a-z]/, '')
   end
+  
+  def clippy(text)
+    render :partial => "shared/clippy", :locals => {:text => text}
+  end
 end
