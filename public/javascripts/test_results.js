a8973440261c35686fab40c3278d682af701c087
Matte Noble <me@mattenoble.com>
2011-02-15 22:42:51 -0500

    Adding github style tooltip to copy output button

--- public/javascripts/test_results.js
+++ public/javascripts/test_results.js
@@ -1,1 +1,21 @@
-App.TestResults = {}
+App.TestResults = {
+  copy: null,
+
+  init: function() {
+    this.copy = $('#copy');
+    this.load_events();
+  },
+
+  load_events: function() {
+    this.copy.tipsy({ gravity: 's' });
+    this.copy.mousedown(App.apply(this.copy_mousedown, this));
+  },
+
+  copy_mousedown: function() {
+    this.copy
+      .attr('title', 'copied!')
+      .tipsy('hide')
+      .tipsy('show')
+      .attr('title', 'copy to clipboard');
+  }
+}
